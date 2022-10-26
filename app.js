f8e775205bc148fad78e6a9f1e74197e40776d48
Dhrupad <thedhrupad@gmail.com>
2022-10-26 12:22:18 +0530

    google auth changed

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://iiits-secrets-anonymous.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 
   },
